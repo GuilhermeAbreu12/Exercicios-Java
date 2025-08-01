f824a694c607c4d67a4514776ac5e836de90f517
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-01 12:30:02 -0300

    feat: add ex_010.java

---
+++ level2/ex_010.java
@@ -0,0 +1,20 @@
+/* Maior de Idade: Peça a idade do usuário e informe se ele é maior de idade (>= 18 anos). */
+package level2;
+import java.util.Scanner;
+public class ex_010 {
+    public static void main(String[] args){
+        Scanner recebe = new Scanner(System.in);
+        int idade;
+
+        System.out.print("Digite sua idade: ");
+        idade = recebe.nextInt();
+
+        System.out.print("Você é ");
+        
+        if (idade>=18) System.out.print("maior");
+        else System.out.print("menor");
+
+        System.out.print(" de idade");
+        recebe.close();
+    }
+}
