24e786391998d1b3998c3fc7e6168ee0ae005df4
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-01 09:38:01 -0300

    feat: add ex_007.java

---
+++ level1/ex_007.java
@@ -0,0 +1,17 @@
+/* Peça um número inteiro e mostre na tela o seu antecessor e o seu sucessor. */
+import java.util.Scanner;
+public class ex_007 {
+    public static void main(String[] args){
+        Scanner recebe = new Scanner(System.in);
+        int num, antecessor, sucessor;
+
+        System.out.print("Digite um número: ");
+        num = recebe.nextInt();
+
+        sucessor = num + 1;
+        antecessor = num - 1;
+        System.out.print("\nNúmero: "+num+"\nAntecessor: "+antecessor+"\nSucessor: "+sucessor);
+
+        recebe.close();
+    }
+}
