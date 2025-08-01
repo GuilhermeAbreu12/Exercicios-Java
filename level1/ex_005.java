20d480f49656b2f7dd1df07f41dbc3cb3eddce81
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-01 09:37:46 -0300

    feat: add ex_005.java

---
+++ level1/ex_005.java
@@ -0,0 +1,18 @@
+/* Crie um programa que converta um valor em Reais (R$) para Dólares (US$). Peça ao usuário o valor em Reais e a cotação do dólar. */
+import java.util.Scanner;
+public class ex_005 {
+    public static void main(String[] args){
+        Scanner recebe = new Scanner(System.in);
+        double reais, cotacao, dolares;
+
+        System.out.print("Digite o valor em Reais: R$ ");
+        reais = recebe.nextDouble();
+
+        System.out.print("Digite a cotação do Dólar: R$ ");
+        cotacao = recebe.nextDouble();
+        dolares = (reais*cotacao);
+
+        System.out.print("R$ "+reais+"\nUS$ "+dolares);
+        recebe.close();
+    }
+}
