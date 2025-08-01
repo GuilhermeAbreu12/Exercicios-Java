6ac6855fabdaf00701cc44712e77cba42db70a31
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-01 12:29:54 -0300

    feat: add ex_009.java

---
+++ level2/ex_009.java
@@ -0,0 +1,17 @@
+/* Par ou Ímpar: Peça um número e verifique se ele é par ou ímpar. */
+package level2;
+import java.util.Scanner;
+public class ex_009 {
+    public static void main(String[] args){
+        Scanner recebe = new Scanner(System.in);
+        double num;
+
+        System.out.print("Digite um número: ");
+        num = recebe.nextDouble();
+
+        System.out.print("O número é ");
+        if (num%2 == 0) System.out.print("par"); 
+        else System.out.print("ímpar");
+        recebe.close();
+    }
+}
