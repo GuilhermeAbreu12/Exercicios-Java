426e03c8d189fd5734f9f643a332e3c1344ce5e3
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-01 12:30:08 -0300

    feat: add ex_011.java

---
+++ level2/ex_011.java
@@ -0,0 +1,27 @@
+/* Maior de Dois Números: Peça dois números e indique qual deles é o maior. */
+package level2;
+import java.util.Scanner;
+public class ex_011 {
+    public static void main(String[] args){
+        Scanner recebe = new Scanner(System.in);
+        double num1, num2;
+        boolean maior;
+
+        System.out.print("Digite um número: ");
+        num1 = recebe.nextDouble();
+        System.out.print("Digite outro número: ");
+        num2 = recebe.nextDouble();
+        
+        maior = num1 != num2;
+
+        if (maior){
+            System.out.print("O maior número é o ");
+            
+            if (num1 > num2) System.out.print(num1);
+            else System.out.print(num2);
+        }
+        else System.out.print("Os números são iguais");
+            
+        recebe.close();
+    }
+}
