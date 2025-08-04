2664eab8d07f2c242eff55a223f943e21be9290e
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-04 20:40:04 -0300

    feat: add Ex016_Contar1a10.java

---
+++ level3_lacos_repeticao/Ex016_Contar1a10.java
@@ -0,0 +1,11 @@
+/* Contagem até 10: Use um laço for para imprimir os números de 1 a 10. */
+package level3_lacos_repeticao;
+public class Ex016_Contar1a10 {
+    public static void main(String[] args){
+        int i;
+        System.out.println("\n============== Contagem até 10 ==============");
+        System.out.println("De 1 ao 10, um de cada vez\n");
+        for (i = 1; i<=10; i++)
+            System.out.println(i);       
+    }
+}
